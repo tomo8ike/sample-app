d92dc8ef64a463ab08472b48616ec6eae9b5f1f0
tomo8ike <tomo8ike@gmail.com>
2020-02-22 07:12:52 +0000

    E1,E2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -59,4 +59,17 @@ class UsersController < ApplicationController
     def set_user
       @user = User.find(params[:id])
     end
+    
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "ログインしてください。"
+        redirect_to login_url
+      end
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless @user == current_user
+    end    
 end
